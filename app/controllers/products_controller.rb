6831352a101b55c01eda4a4d37baf4ea3537c902
周文耿 <412582902@qq.com>
2017-10-05 18:19:03 +0800

    test add add_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    flash[:notice] = "测试加入购物车"
+    redirect_to :back
+  end
 end
